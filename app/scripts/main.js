d3b595f26e4cebf07513be302ab526c53f90be4a
Chandra Sekhar Veera <veeracs@icloud.com>
2013-10-01 14:51:39 -0400

    post messaging for ccUpsell screens

---
+++ app/scripts/main.js
@@ -0,0 +1,29 @@
+(function(){
+	var requestPayload = {next: true};
+	$('#submitPayment').on('click', function(){
+		//	always set the domain value
+		window.location = 'http://ist-cveera-6065:5000/ccUpsellConfirm.html?domain=http://IST-cveera-6065:9001/';
+	});
+
+	$('#billMeLater').on('click', function(){
+		//	post message to navigate to next view in the parent
+		parent.postMessage(JSON.stringify(requestPayload), getQueryParams('domain'));
+	});
+
+	$('#finish').on('click', function(){
+		//	post message to navigate to next view in the parent
+		parent.postMessage(JSON.stringify(requestPayload), getQueryParams('domain'));
+	});
+
+	function getQueryParams(param) {
+		var queryStr = window.location.search.slice(1),
+		params = queryStr.split('&');
+		for(var i=0; i < params.length; i++){
+			var pair = params[i].split('=');
+			if(pair[0] === param) {
+				return pair[1];
+			}
+		}
+		return false;
+	}
+})();
